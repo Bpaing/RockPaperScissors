778ca8f0f6bf723710878e72e9d498263d08daee
Brendan Paing <brendanpaing@gmail.com>
2022-05-21 22:21:44 -0700

    finish RPS implementation

---
+++ RPS.js
@@ -0,0 +1,34 @@
+function computerPlay() {
+    let choices = ["rock", "paper", "scissors"];
+    return choices[Math.floor((Math.random()*3))];
+}
+
+function playRound(playerSelection, computerSelection) {
+    if (playerSelection === computerSelection) 
+        return `draw: ${playerSelection} does not beat ${computerSelection}`;
+    else if (playerSelection === 'rock') {
+        if (computerSelection === 'scissors')
+            return `win: ${playerSelection} beats ${computerSelection}`;
+        return `loss: ${computerSelection} beats ${playerSelection}`;
+    }
+    else if (playerSelection === 'paper') {
+        if (computerSelection === 'rock')
+            return `win: ${playerSelection} beats ${computerSelection}`;
+        return `loss: ${computerSelection} beats ${playerSelection}`;
+    }
+    else if (playerSelection === 'scissors') {
+        if (computerSelection === 'paper')
+            return `win: ${playerSelection} beats ${computerSelection}`;
+        return `loss: ${computerSelection} beats ${playerSelection}`;
+    }
+}
+
+function game() {
+    for (let i = 0; i < 5; i++) {
+        let player = prompt(`Round ${i+1}- Choice: `).toLowerCase();
+        let computer = computerPlay();
+        console.log(playRound(player, computer));
+    }
+}
+
+game()
