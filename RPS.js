9dee4b7a0b3b66717efbf40c712678634d66e293
Brendan Paing <brendanpaing@gmail.com>
2022-05-27 17:42:02 -0700

    Modify playRound to only return draw, win, or loss

--- RPS.js
+++ RPS.js
@@ -4,34 +4,61 @@ function computerPlay() {
 }
 
 function playRound(playerSelection, computerSelection) {
-    if (playerSelection === computerSelection) 
-        return `draw: ${playerSelection} does not beat ${computerSelection}`;
+    if (playerSelection === computerSelection) {
+        return 'draw';
+    }
     else if (playerSelection === 'rock') {
-        if (computerSelection === 'scissors')
-            return `win: ${playerSelection} beats ${computerSelection}`;
-        return `loss: ${computerSelection} beats ${playerSelection}`;
+        if (computerSelection === 'scissors') {
+            return 'win';
+        } 
+        return 'loss';
     }
     else if (playerSelection === 'paper') {
-        if (computerSelection === 'rock')
-            return `win: ${playerSelection} beats ${computerSelection}`;
-        return `loss: ${computerSelection} beats ${playerSelection}`;
+        if (computerSelection === 'rock') {
+            return 'win';
+        }
+        return 'loss';
     }
     else if (playerSelection === 'scissors') {
-        if (computerSelection === 'paper')
-            return `win: ${playerSelection} beats ${computerSelection}`;
-        return `loss: ${computerSelection} beats ${playerSelection}`;
+        if (computerSelection === 'paper') {
+            return 'win';
+        }
+        return 'loss';
     }
 }
 
-function playOnClick(e) {
-    const choice = this.textContent.toLowerCase();
-    const outcome = playRound(choice, computerPlay());
+function addContent(div, wins, losses) {
+    const ul = document.createElement('ul');
+    const win = document.createElement('li');
+    win.id = 'win';
+    const loss = document.createElement('li');
+    loss.id = 'loss';
+    ul.appendChild(win);
+    ul.appendChild(loss);
+}
 
-    const result = document.querySelector('div.result');
-    console.log(result);
-    const p = document.createElement('p');
-    p.textContent = outcome;
-    result.appendChild(p);
+function playOnClick(e) {
+    const player = this.textContent.toLowerCase();
+    const computer = computerPlay();
+    const outcome = playRound(player, computer);
+
+    const ul = document.createElement('ul');
+    const playerLi = document.createElement('li');
+    playerLi.textContent = `You chose: ${player}`;
+    const computerLi = document.createElement('li');
+    computerLi.textContent = `Computer chose: ${computer}`;
+    const outcomeLi = document.createElement('li');
+    outcomeLi.textContent = outcome;
+    ul.appendChild(playerLi);
+    ul.appendChild(computerLi);
+    ul.appendChild(outcomeLi);
+
+    const scoreboard = document.querySelector('div.scoreboard');
+    if (scoreboard.hasChildNodes()) {
+        scoreboard.replaceChild(ul, scoreboard.firstChild);
+    } else {
+        scoreboard.appendChild(ul);
+    }
 }
 
 const rps = document.querySelectorAll('button');
